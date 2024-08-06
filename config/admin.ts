1c1ace582dec68544fa75c63faefab555197d6cc
Zeb Sola <zebsola@gmail.com>
2024-08-05 20:01:58 -0700

    add random salts

--- config/admin.ts
+++ config/admin.ts
@@ -3,11 +3,11 @@ export default ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', crypto.randomBytes(16).toString('base64')),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', crypto.randomBytes(16).toString('base64')),
     },
   },
   flags: {
