5948a3f975a63222b699a4476e6f477ad0541d3c
Zeb Sola <zebsola@gmail.com>
2024-08-05 20:08:39 -0700

    use fixed salt value

--- config/admin.ts
+++ config/admin.ts
@@ -3,11 +3,11 @@ export default ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT', crypto.randomBytes(16).toString('base64')),
+    salt: env('API_TOKEN_SALT', 'cbb96998bf388cd8f90027ea2b16971c'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT', crypto.randomBytes(16).toString('base64')),
+      salt: env('TRANSFER_TOKEN_SALT', 'cbb96998bf388cd8f90027ea2b16971c'),
     },
   },
   flags: {
